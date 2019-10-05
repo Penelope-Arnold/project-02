41a91b66ac2178db13f02ffce9ea2165cc1416b1
Esther Cho <esthercho@Esthers-MacBook-Air.local>
2019-10-05 11:40:34 -0600

    ignore

--- views/js/cms.js
+++ views/js/cms.js
@@ -1,149 +1,149 @@
-$(document).ready(function() {
+// $(document).ready(function() {
   // Getting jQuery references to the post body, title, form, and user select
-  var cityInput = $("#city");
-  var countryInput = $("#country");
-  var categoryInput = $("#category");
-  var descriptionInput = $("#description");
-  var photoInput = $("#photo");
-  var cmsForm = $("#cms");
+  // var cityInput = $("#city");
+  // var countryInput = $("#country");
+  // var categoryInput = $("#category");
+  // var descriptionInput = $("#description");
+  // var photoInput = $("#photo");
+  // var cmsForm = $("#cms");
   // Adding an event listener for when the form is submitted
-  $(cmsForm).on("submit", handleFormSubmit);
+  // $(cmsForm).on("submit", handleFormSubmit);
   // Gets the part of the url that comes after the "?" (which we have if we're updating a post)
-  var url = window.location.search;
-  var postId;
-  var userId;
+  // var url = window.location.search;
+  // var postId;
+  // var userId;
   // Sets a flag for whether or not we're updating a post to be false initially
-  var updating = false;
+  // var updating = false;
 
   // If we have this section in our url, we pull out the post id from the url
   // In '?post_id=1', postId is 1
-  if (url.indexOf("?post_id=") !== -1) {
-    postId = url.split("=")[1];
-    getPostData(postId, "post");
-  }
+  // if (url.indexOf("?post_id=") !== -1) {
+  //   postId = url.split("=")[1];
+  //   getPostData(postId, "post");
+  // }
   // Otherwise if we have an user_id in our url, preset the user select box to be our User
-  else if (url.indexOf("?user_id=") !== -1) {
-    userId = url.split("=")[1];
-  }
+  // else if (url.indexOf("?user_id=") !== -1) {
+  //   userId = url.split("=")[1];
+  // }
 
   // Getting the users, and their posts
-  getUsers();
+  // getUsers();
 
   // A function for handling what happens when the form to create a new post is submitted
-  function handleFormSubmit(event) {
-    event.preventDefault();
+  // function handleFormSubmit(event) {
+  //   event.preventDefault();
     // Wont submit the post if we are missing a body, title, or user
-    if (!cityInput.val().trim() || !countryInput.val().trim() || !categoryInput.val().trim() ||!descriptionInput.val().trim() || !photoInput.val().trim() ||!userSelect.val()) {
-      return;
-    }
+    // if (!cityInput.val().trim() || !countryInput.val().trim() || !categoryInput.val().trim() ||!descriptionInput.val().trim() || !photoInput.val().trim() ||!userSelect.val()) {
+    //   return;
+    // }
     // Constructing a newPost object to hand to the database
-    var newPost = {
-      city: cityInput
-        .val()
-        .trim(),
-      country: countryInput
-        .val()
-        .trim(),
-      category: categoryInput
-        .val()
-        .trim(),
-      description: descriptionInput
-        .val()
-        .trim(),
-      photo: photoInput
-        .val()
-        .trim(),
-      UserId: userSelect.val()
+    // var newPost = {
+    //   city: cityInput
+    //     .val()
+    //     .trim(),
+    //   country: countryInput
+    //     .val()
+    //     .trim(),
+    //   category: categoryInput
+    //     .val()
+    //     .trim(),
+    //   description: descriptionInput
+    //     .val()
+    //     .trim(),
+    //   photo: photoInput
+    //     .val()
+    //     .trim(),
+    //   UserId: userSelect.val()
 
-    };
+    // };
 
     // If we're updating a post run updatePost to update a post
     // Otherwise run submitPost to create a whole new post
-    if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
-    }
-    else {
-      submitPost(newPost);
-    }
-  }
+  //   if (updating) {
+  //     newPost.id = postId;
+  //     updatePost(newPost);
+  //   }
+  //   else {
+  //     submitPost(newPost);
+  //   }
+  // }
 
   // Submits a new post and brings user to blog page upon completion
-  function submitPost(post) {
-    $.post("/api/posts", post, function() {
-      window.location.href = "/blog";
-    });
-  }
+  // function submitPost(post) {
+  //   $.post("/api/posts", post, function() {
+  //     window.location.href = "/blog";
+  //   });
+  // }
 
   // Gets post data for the current post if we're editing, or if we're adding to an user's existing posts
-  function getPostData(id, type) {
-    var queryUrl;
-    switch (type) {
-    case "post":
-      queryUrl = "/api/posts/" + id;
-      break;
-    case "user":
-      queryUrl = "/api/users/" + id;
-      break;
-    default:
-      return;
-    }
-    $.get(queryUrl, function(data) {
-      if (data) {
+  // function getPostData(id, type) {
+  //   var queryUrl;
+  //   switch (type) {
+  //   case "post":
+  //     queryUrl = "/api/posts/" + id;
+  //     break;
+  //   case "user":
+  //     queryUrl = "/api/users/" + id;
+  //     break;
+  //   default:
+  //     return;
+    // }
+    // $.get(queryUrl, function(data) {
+    //   if (data) {
         // console.log(data.UserId || data.id);
         // If this post exists, prefill our cms forms with its data
-        cityInput.val(data.city);
-        countryInput.val(data.country);
-        categoryInput.val(data.category); 
-        descriptionInput.val(data.description);
-        photoInput.val(data.photo);
-        userId = data.UserId || data.id;
+        // cityInput.val(data.city);
+        // countryInput.val(data.country);
+        // categoryInput.val(data.category); 
+        // descriptionInput.val(data.description);
+        // photoInput.val(data.photo);
+        // userId = data.UserId || data.id;
         // If we have a post with this id, set a flag for us to know to update the post
         // when we hit submit
-        updating = true;
-      }
-    });
-  }
+  //       updating = true;
+  //     }
+  //   });
+  // }
 
   // A function to get Users and then render our list of Users
-  function getUsers() {
-    $.get("/api/users", renderUserList);
-  }
+  // function getUsers() {
+  //   $.get("/api/users", renderUserList);
+  // }
   // Function to either render a list of users, or if there are none, direct the user to the page
   // to create an user first
-  function renderUserList(data) {
-    if (!data.length) {
-      window.location.href = "/users";
-    }
-    $(".hidden").removeClass("hidden");
-    var rowsToAdd = [];
-    for (var i = 0; i < data.length; i++) {
-      rowsToAdd.push(createUserRow(data[i]));
-    }
-    userSelect.empty();
+  // function renderUserList(data) {
+  //   if (!data.length) {
+  //     window.location.href = "/users";
+  //   }
+  //   $(".hidden").removeClass("hidden");
+  //   var rowsToAdd = [];
+  //   for (var i = 0; i < data.length; i++) {
+  //     rowsToAdd.push(createUserRow(data[i]));
+  //   }
+  //   userSelect.empty();
     // console.log(rowsToAdd);
     // console.log(userSelect);
-    userSelect.append(rowsToAdd);
-    userSelect.val(userId);
-  }
+  //   userSelect.append(rowsToAdd);
+  //   userSelect.val(userId);
+  // }
 
   // Creates the user options in the dropdown
-  function createUserRow(user) {
-    var listOption = $("<option>");
-    listOption.attr("value", user.id);
-    listOption.text(user.name);
-    return listOption;
-  }
+  // function createUserRow(user) {
+  //   var listOption = $("<option>");
+  //   listOption.attr("value", user.id);
+  //   listOption.text(user.name);
+  //   return listOption;
+  // }
 
   // Update a given post, bring user to the blog page when done
-  function updatePost(post) {
-    $.ajax({
-      method: "PUT",
-      url: "/api/posts",
-      data: post
-    })
-      .then(function() {
-        window.location.href = "/blog";
-      });
-  }
-});
+//   function updatePost(post) {
+//     $.ajax({
+//       method: "PUT",
+//       url: "/api/posts",
+//       data: post
+//     })
+//       .then(function() {
+//         window.location.href = "/blog";
+//       });
+//   }
+// });
