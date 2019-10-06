e2ea171fb607c543729bc7710b47cf66502506cf
Penelope A <penelope.arnold@du.edu>
2019-10-05 18:34:38 -0600

    heroku things no code changes

--- config/connection.js
+++ config/connection.js
@@ -12,14 +12,17 @@ var connection;
 // } else {
 
 console.log(process.env.DB_PASS);
-connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else { 
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASS,
   database: "blogger"
 });
-// }
+}
 
 // Make connection.
 connection.connect(function(err) {
