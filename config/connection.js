b240d714f306381e58ca9eacfb73ac44176c1fb3
Paul Graven <graven.paul@gmail.com>
2019-10-06 13:03:28 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,19 +7,18 @@ require("dotenv").config();
 var mysql = require("mysql");
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-
-console.log(process.env.DB_PASS);
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "blogger"
-});
-// }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  console.log(process.env.DB_PASS);
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "blogger"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
