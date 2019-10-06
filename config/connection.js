b31e27f0f61fd004240599fc340b54c801732912
Penelope A <penelope.arnold@du.edu>
2019-10-06 13:43:28 -0600

    not relevant

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 var mysql = require("mysql");
 var connection;
 
-<<<<<<< HEAD
 // if (process.env.JAWSDB_URL) {
 //   connection = mysql.createConnection(process.env.JAWSDB_URL);
 // } else {
@@ -23,19 +22,6 @@ if (process.env.JAWSDB_URL) {
   password: process.env.DB_PASS,
   database: "blogger"
 });
-=======
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  console.log(process.env.DB_PASS);
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "blogger"
-  });
->>>>>>> 658f2f35b44c3f454f21b0c65442f7b2cd9f14a0
 }
 
 // Make connection.
