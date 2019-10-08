6a1c3ea9475a19c6506b4a30429c76b0f91c7ade
Paul Graven <graven.paul@gmail.com>
2019-10-08 16:14:42 -0600

    changed Login to login

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const session = require('express-session');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const session = require("express-session");
 
 //User model
-const db = require('../models');
+const db = require("../models");
 
 //Login Page
-router.get('/login', (req, res) => res.render('Login'));
+router.get("/login", (req, res) => res.render("login"));
 
 //Register Page
-router.get('/register', (req, res) => res.render('Register'));
+router.get("/register", (req, res) => res.render("Register"));
 
 // Register
-router.post('/register', (req, res) => {
-
+router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
+    errors.push({ msg: "Please enter all fields" });
   }
 
   if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
+    errors.push({ msg: "Passwords do not match" });
   }
 
   if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
+    errors.push({ msg: "Password must be at least 6 characters" });
   }
 
   if (errors.length > 0) {
-    res.render('register', {
+    res.render("register", {
       errors,
       name,
       email,
@@ -45,8 +44,8 @@ router.post('/register', (req, res) => {
       where: { email: email }
     }).then(user => {
       if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('register', {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
           errors,
           name,
           email,
@@ -58,47 +57,49 @@ router.post('/register', (req, res) => {
           name: name,
           email: email,
           password: password
-        }
+        };
 
-        db.User.create(newUser).then(user => {
-          //Hash Password
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(user.password, salt, (err, hash) => {
-              if (err) throw err;
-              //Set password to hashed
-              user.password = hash;
-              //Save user
-              user.save()
-                .then(user => {
-                  req.flash(
-                    'success_msg',
-                    'You are now registered and can log in'
-                  );
-                  res.redirect('/users/login');
-                })
-                .catch(err => console.log(err));
+        db.User.create(newUser)
+          .then(user => {
+            //Hash Password
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) throw err;
+                //Set password to hashed
+                user.password = hash;
+                //Save user
+                user
+                  .save()
+                  .then(user => {
+                    req.flash(
+                      "success_msg",
+                      "You are now registered and can log in"
+                    );
+                    res.redirect("/users/login");
+                  })
+                  .catch(err => console.log(err));
+              });
             });
-          });
-        }).catch(err => {
-        })
+          })
+          .catch(err => {});
       }
     });
   }
 });
 
 //Login Handle
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/users/login',
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
     failureFlash: true
   })(req, res, next);
 });
 
 //Logout Handle
-router.get('/logout', function (req, res){
-  req.session.destroy(function (err) {
-   res.redirect('/users/login');
+router.get("/logout", function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect("/users/login");
   });
 });
 
