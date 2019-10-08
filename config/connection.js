545fb6b02321142dbdfedba79af194ddad0b63ea
Paul Graven <graven.paul@gmail.com>
2019-10-08 10:35:09 -0600

    updated connection.js with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,14 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var connection;
-var JAWSDB_URL =
-  "mysql://y0oer6sd4wtdtjdm:dqvlrliure82or5q@s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/imh1whbm6zy1h310";
 
-// if JAWSDB_URL exists, then Database is JawsDB on Heroku
-if (JAWSDB_URL) {
-  connection = mysql.createConnection(JAWSDB_URL);
-}
-// else connect to local MySQL database
-else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "blogger"
-  });
-}
+connection = mysql.createConnection({
+  host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "y0oer6sd4wtdtjdm",
+  password: dqvlrliure82or5q,
+  database: "imh1whbm6zy1h310"
+});
 
 // Make connection.
 connection.connect(function(err) {
@@ -32,23 +23,18 @@ connection.connect(function(err) {
 module.exports = connection;
 
 //Create Connection to database with Sequelize
-if (JAWSDB_URL) {
-  var Sequelize = require("sequelize");
-  sequelize = new Sequelize(JAWSDB_URL, {
+
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize(
+  "imh1whbm6zy1h310",
+  "y0oer6sd4wtdtjdm",
+  "dqvlrliure82or5q",
+  {
+    host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
     dialect: "mysql"
-  });
-} else {
-  var Sequelize = require("sequelize");
-  var sequelize = new Sequelize(
-    "imh1whbm6zy1h310",
-    "y0oer6sd4wtdtjdm",
-    "dqvlrliure82or5q",
-    {
-      host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      port: 3306,
-      dialect: "mysql"
-  });
-}
+  }
+);
 
 //export object
 module.exports = sequelize;
